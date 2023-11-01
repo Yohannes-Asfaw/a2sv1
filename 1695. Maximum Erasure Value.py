7a2812caed070d162da0743b5920ccf407630113
Yohannes Asfaw <90513510+Yohannes-Asfaw@users.noreply.github.com>
2023-11-01 13:33:54 +0300

    Create 1695. Maximum Erasure Value.py

---
+++ 1695. Maximum Erasure Value.py
@@ -0,0 +1,17 @@
+  def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        dic=defaultdict(int)
+        left=0
+        maxsum=0
+        sum1=0
+        for right in range(len(nums)):
+            while nums[right] in dic:
+                sum1-=nums[left]
+                del dic[nums[left]]
+                left+=1 
+            dic[nums[right]]=1
+            sum1+=nums[right]
+            maxsum=max(sum1,maxsum)
+        return maxsum
+            
+
+        
